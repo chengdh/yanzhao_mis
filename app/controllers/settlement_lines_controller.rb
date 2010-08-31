cc803bc7852f79878320ea79397e5d50e6d8e970
chengdh <cheng.donghui@gmail.com>
2010-08-31 18:00:57 +0800

    fixed some settlement function bug and views

--- app/controllers/settlement_lines_controller.rb
+++ app/controllers/settlement_lines_controller.rb
@@ -1,7 +1,7 @@
 class SettlementLinesController < BaseController
   def index
-    @settlement_lines = @search.paginate :page => params[:page],:order => "created_at DESC,name"
-    @group_settlement_lines = @settlement_lines.group_by {|line| line.settlement.org.name}
+    @settlement_lines = @search.paginate :page => params[:page],:order => "name,created_at DESC"
+    #@group_settlement_lines = @settlement_lines.group_by {|line| line.settlement.org.name}
     @sum = @search.sum(:value)
   end
 
