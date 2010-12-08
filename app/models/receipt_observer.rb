e719aa2fc327629cfa21bc96f8ba52a4bd472ebc
chengdh <cheng.donghui@gmail.com>
2010-12-08 17:22:25 +0800

    修改了入库单明细数量为零时的错误

--- app/models/receipt_observer.rb
+++ app/models/receipt_observer.rb
@@ -5,14 +5,16 @@ class ReceiptObserver < ActiveRecord::Observer
     #只有审核后才进行处理
     return if receipt.state == MaterialInout::STATE_DRAFT
     receipt.material_inout_lines.each do |line|
-      m_storage = MStorage.warehouse_id_is(receipt.warehouse).material_id_is(line.material).first
-      #更新均价
-      m_storage.avg_price = (m_storage.qty*m_storage.avg_price + line.price * line.qty)/(m_storage.qty + line.qty)
-      #更新库存
-      m_storage.qty += line.qty
-      #设置库存报警
-      m_storage.alert = (line.material.min_count > 0 && m_storage.qty <= line.material.min_count) ? true : false
-      m_storage.save
+      if line.qty > 0
+        m_storage = MStorage.warehouse_id_is(receipt.warehouse).material_id_is(line.material).first
+        #更新均价
+        m_storage.avg_price = (m_storage.qty*m_storage.avg_price + line.price * line.qty)/(m_storage.qty + line.qty)
+        #更新库存
+        m_storage.qty += line.qty
+        #设置库存报警
+        m_storage.alert = (line.material.min_count > 0 && m_storage.qty <= line.material.min_count) ? true : false
+        m_storage.save
+      end
     end
   end
 end
